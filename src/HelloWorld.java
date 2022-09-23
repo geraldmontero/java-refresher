2db7bae854901b2cc062265e1f51736b0a7278a9
Gerald Montero <geraldmontero12@gmail.com>
2022-09-23 10:55:49 -0500

    friday refresh

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.print("Hello ");
-        System.out.print("world");
+//        System.out.println("Hello world");
+//        System.out.print("Hello ");
+//        System.out.print("world");
+//        Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console.
+        int myFavoriteNumber = 7;
+        System.out.println(myFavoriteNumber);
+  //    Create a String variable named myString and assign a string value to it, then print the variable out to the console.
+        String myString = "hello";
+
     }
 }
