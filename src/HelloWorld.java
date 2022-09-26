0c602a50c6236547338dfb03f0fadfc7f8dbfb9a
Gerald Montero <geraldmontero12@gmail.com>
2022-09-26 18:46:35 -0500

    Java syntax and variables refresher

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,8 +6,45 @@ public class HelloWorld {
 //        Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console.
         int myFavoriteNumber = 7;
         System.out.println(myFavoriteNumber);
-  //    Create a String variable named myString and assign a string value to it, then print the variable out to the console.
+//         Create a String variable named myString and assign a string value to it, then print the variable out to the console.
         String myString = "hello";
+        System.out.println(myString);
+//      Declare an long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens?
+//        long myNumber = 123;
+//        System.out.println(myNumber);
+//
+////        int x = 5;
+////        System.out.println(x++);
+////        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        Rewrite the following expressions using the relevant shorthand assignment operators:
+//
+//
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+//
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+//
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(y);
+        System.out.println(x);
+
+
+
 
     }
 }
