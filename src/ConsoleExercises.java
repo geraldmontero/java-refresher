b3f98679c2785cd8ae886e0c669ed92054ee9203
Gerald Montero <geraldmontero12@gmail.com>
2022-09-26 19:40:24 -0500

    console io refresher

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,4 +1,5 @@
 import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class ConsoleExercises {
 
@@ -10,7 +11,27 @@ public class ConsoleExercises {
 //
 //
 //The value of pi is approximately 3.14.
-        System.out.format("The Value of  pi is %.2f",pi);
+//        System.out.format("The Value of  pi is %.2f",pi);
+
+//    1.    Prompt a user to enter a integer and store that value in an int variable using the nextInt method.
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter an Integer: ");
+//        int userInput = scanner.nextInt();
+//
+//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+
+//        2.Prompt a user to enter 3 words, and store each of them in a separate variable. Then, display them back in the console, each on a newline.
+//
+//What happens if you enter less than 3 words?
+//What happens if you enter more than 3 words?
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 3 words ");
+        String userIn = scanner.next();
+        String userIn2 = scanner.next();
+        String userIn3 = scanner.next();
+        System.out.println(userIn +" "+ userIn3 +" "+ userIn2);
+
 
     }
 }
