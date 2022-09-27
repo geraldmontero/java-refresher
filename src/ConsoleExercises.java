d5cc136464e9e044c04ec9f342a7a485fbc2f05e
Gerald Montero <geraldmontero12@gmail.com>
2022-09-26 19:19:12 -0500

    console io refresher

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,16 @@
+import java.text.DecimalFormat;
+
+public class ConsoleExercises {
+
+
+
+    public static void main(String[] args) {
+        double pi = 3.14159;
+//   Write some Java code that uses the variable pi to output the following:
+//
+//
+//The value of pi is approximately 3.14.
+        System.out.format("The Value of  pi is %.2f",pi);
+
+    }
+}
